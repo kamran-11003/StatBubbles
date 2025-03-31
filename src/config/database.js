5916734f6c62d5e726dd1aeb3fffccefdea46f62
kamran-11003 <kamranchohan740@gmail.com>
2025-03-31 09:17:23 +0500

    fixes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://kamran:kamran@cluster0.2ao9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
